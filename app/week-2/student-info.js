7df16f6e569624fd0029c7fd5bfd262a13105c2d
Ahmad Baker <ahmadqadoomy79@gmail.com>
2024-09-07 12:28:50 -0600

    first commit showing my week-2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+function StudentInfo() {
+     return (
+            <div>
+                <p>Ahmad Baker</p>
+                <Link href="https://github.com/AhmadBaker1">
+                    https://github.com/AhmadBaker1
+                </Link>
+            </div>
+     )
+    }
+ 
+ 
+
+ export default StudentInfo;
